4d1e8010c7d51d97050f86a95dafe6cddd558fc8
rachit2501 <rac.sri25@gmail.com>
2020-07-01 22:04:46 +0530

    cache size limiting and flaccback fixes

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
-const cacheName = "site-static-v2";
-const dynamiCache = "site-dynamic-1";
+const staticCacheName = "site-static-v2";
+const dynamicCacheName = "site-dynamic-v1";
 const assets = [
   "/",
   "/index.html",
@@ -14,8 +14,17 @@ const assets = [
   "/pages/fallback.html",
 ];
 
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 self.addEventListener("install", (evt) => {
-  //console.log('service worker installed');
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
       console.log("caching shell assets");
@@ -25,13 +34,12 @@ self.addEventListener("install", (evt) => {
 });
 
 self.addEventListener("activate", (evt) => {
-  console.log("Activated");
-
   evt.waitUntil(
     caches.keys().then((keys) => {
+      //console.log(keys);
       return Promise.all(
         keys
-          .filter((key) => key !== cacheName && key !== dynamiCache)
+          .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
           .map((key) => caches.delete(key))
       );
     })
@@ -39,24 +47,26 @@ self.addEventListener("activate", (evt) => {
 });
 
 self.addEventListener("fetch", (evt) => {
-  console.log("fetching");
-
   evt.respondWith(
-    caches.match(evt.request).then((cache) => {
-      return (
-        cache ||
-        fetch(evt.request)
-          .then((fetches) => {
-            return caches.open(dynamiCache).then((cache) => {
-              cache.put(evt.request.url, fetches.clone());
-              return fetches;
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCacheName).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              // check cached items size
+              limitCacheSize(dynamicCacheName, 15);
+              return fetchRes;
             });
           })
-          .catch(() => {
-            if (evt.request.url.indexOf(".html") > -1)
-              caches.match("/pages/fallback.html");
-          })
-      );
-    })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/pages/fallback.html");
+        }
+      })
   );
 });
